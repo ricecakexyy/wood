513cadb5239c3aa783fe2cd722af293a6b0ade75
735308107@qq.com <735308107@qq.com>
2022-12-09 19:47:15 +0800

    a

---
+++ camera_ui/build/generated/source/buildConfig/debug/com/baidu/ai/edge/ui/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.baidu.ai.edge.ui;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.baidu.ai.edge.ui";
+  public static final String BUILD_TYPE = "debug";
+}
